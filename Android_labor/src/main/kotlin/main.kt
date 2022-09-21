32ffd6e6d23f194bcd874b49dbde471c6a1dd161
doramagyari <79280364+doramagyari@users.noreply.github.com>
2022-09-21 10:49:13 +0300

    test commit

---
+++ Android_labor/src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    val name : String = "Magyari Dora"
+    //name = "Magyari Dora"
+
+    val number1 = 2
+    val number2 = 5
+    println("$number1 + $number2 = ${number1 + number2}")
+}
